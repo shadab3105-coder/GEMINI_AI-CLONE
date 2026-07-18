afabcd67afbf2a7dec7cbfe9cba12956e592b0e9
Md Shadab Hussain <shadabhussain3105@gmail.com>
2026-07-18 16:55:49 -0500

    Update API key in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyAW8eBUQM2mmgLWXG1tFXHO6QUL9IvD9is";
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AQ.Ab8RN6J3KYdO7YSw_cDXWVL-U-7LZtvWlxNLX0oB9YbRdOUQsQ";
