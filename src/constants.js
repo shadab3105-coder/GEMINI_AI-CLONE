f5b8d46110b0085e90e5aa1754ab57f21ccb5abf
Md Shadab Hussain <shadabhussain3105@gmail.com>
2026-07-18 17:42:51 -0500

    Remove API key from URL in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AQ.Ab8RN6J3KYdO7YSw_cDXWVL-U-7LZtvWlxNLX0oB9YbRdOUQsQ";
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=";
